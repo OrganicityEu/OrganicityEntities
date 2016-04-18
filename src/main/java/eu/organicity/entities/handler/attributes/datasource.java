6f5ae173080a6d137300639d4fd733d740b1ab20
Evangelos Theodoridis <evangelos.theodoridis@intel.com>
2016-04-18 14:20:51 +0100

    datasource

---
+++ src/main/java/eu/organicity/entities/handler/attributes/datasource.java
@@ -0,0 +1,22 @@
+package eu.organicity.entities.handler.attributes;
+
+import eu.organicity.entities.handler.metadata.DatasourceExternal;
+import eu.organicity.entities.handler.metadata.DatasourceInternal;
+import eu.organicity.entities.handler.metadata.Mediatype;
+import eu.organicity.entities.namespace.OrganicityAttributeTypes;
+
+/**
+ * Created by etheodor on 20/10/2015.
+ */
+public class Datasource extends Attribute {
+
+    public Datasource(String value, Boolean internal, String type) {
+        super(OrganicityAttributeTypes.Types.DATASOURCE, value);
+        if (internal == true)
+            this.addMetadata(new DatasourceInternal());
+        else {
+            this.addMetadata(new DatasourceExternal());
+            this.addMetadata(new Mediatype(type));
+        }
+    }
+}
