d294398bc3f8ad1253cce390fe113a166260063f
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2016-05-12 16:59:55 +0300

    add weather station as an EntityType

--- src/main/java/eu/organicity/entities/namespace/OrganicityEntityTypes.java
+++ src/main/java/eu/organicity/entities/namespace/OrganicityEntityTypes.java
@@ -4,7 +4,8 @@ public class OrganicityEntityTypes {
     static public enum EntityType {
         IOT_DEVICE("IotDevice", "urn:oc:entityType:iotdevice"),
         SMARTPHONE("smartphone", "urn:oc:entityType:smartphone"),
-        BOROUGH("borough", "urn:oc:entityType:borough");
+        BOROUGH("borough", "urn:oc:entityType:borough"),
+        WEATHER_STATION("borough", "urn:oc:entityType:weatherstation");
 
         private final String name;
         private final String urn;
