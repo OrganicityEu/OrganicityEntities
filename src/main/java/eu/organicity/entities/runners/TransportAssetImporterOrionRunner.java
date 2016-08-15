6a4917b378d4f4188ffb0012df5287c2e9fa31ee
Evangelos Theodoridis <evangelos.theodoridis@intel.com>
2016-08-15 15:17:04 +0100

    removing obsolete runners
    TransporAsset Orion importer runner added

---
+++ src/main/java/eu/organicity/entities/runners/TransportAssetImporterOrionRunner.java
@@ -0,0 +1,65 @@
+package eu.organicity.entities.runners;
+
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import eu.organicity.entities.handler.converters.AssetToJsonObject;
+import eu.organicity.entities.handler.entities.OrganicityEntity;
+import eu.organicity.entities.importers.ManualCounterImporter;
+import eu.organicity.entities.importers.TransportAPIImporter;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class TransportAssetImporterOrionRunner {
+
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length < 2) {
+            System.err.println("Error: insufficient argument count!");
+            System.err.println("Usage: TransportImporter jsonInputFilename");
+            System.exit(1);
+        }
+
+        String jsonInputFilename = args[0];
+        String url = args[1];
+
+
+        TransportAPIImporter importer = new TransportAPIImporter();
+        List<OrganicityEntity> transportAssets = importer.process(jsonInputFilename);
+        int counter = 0;
+        for (OrganicityEntity tAsset : transportAssets) {
+            JSONObject object = AssetToJsonObject.entityToJsonObject(tAsset);
+            HttpResponse<JsonNode> jsonResponse = Unirest.delete(url + "/" + tAsset.getId() + "?type=" + tAsset.getEntityType())
+                    .header("accept", "application/json")
+                    .header("Content-Type", "application/json")
+                    .asJson();
+
+            if (jsonResponse.getStatus() == HttpStatus.SC_NO_CONTENT) {
+                System.out.println("Asset Deleted:"+tAsset.getId());
+            } else {
+                System.out.println("Asset Not Deleted:"+tAsset.getId());
+            }
+
+            jsonResponse = Unirest.post(url)
+                    .header("accept", "application/json")
+                    .header("Content-Type", "application/json")
+                    .body(object)
+                    .asJson();
+            if (jsonResponse.getStatus() == HttpStatus.SC_CREATED) {
+                counter++;
+                System.out.println("Asset Created:"+tAsset.getId());
+            } else {
+                System.out.println(jsonResponse.getBody());
+                break;
+            }
+        }
+        System.out.println(counter + " assets created out of " + transportAssets.size());
+
+    }
+
+
+}
